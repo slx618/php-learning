3495332a7c76ec5c44f505bb5802c858adc3b6a9
slx618 <slx618@hotmail.com>
2019-05-06 18:02:18 +0800

    commit

---
+++ m/vendor/buildIn/H.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app;
+
+/**
+ * Created by PhpStorm.
+ * User: slx61
+ * Date: 2019/3/4
+ * Time: 17:44
+ */
+class H
+{
+
+    public static function __call()
+    {
+        die('ssss');
+    }
+}
